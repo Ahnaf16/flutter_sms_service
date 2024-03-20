6b6b80f9192d030ec031dc62e7dbdad248489507
Ahnaf Sakil <55395335+Ahnaf16@users.noreply.github.com>
2024-03-20 11:45:13 +0600

    fix plugin setup

--- android/src/main/kotlin/com/marj/flutter_sms_service/FlutterSmsServicePlugin.kt
+++ android/src/main/kotlin/com/marj/flutter_sms_service/FlutterSmsServicePlugin.kt
@@ -1,5 +1,6 @@
 package com.marj.flutter_sms_service
 
+import android.telephony.SubscriptionInfo
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -8,28 +9,59 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
+import android.Manifest
+import android.app.Activity
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.telephony.SmsManager
+import android.telephony.SubscriptionManager
+import android.view.WindowManager
+import androidx.core.app.ActivityCompat.checkSelfPermission
+import io.flutter.Log
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.BinaryMessenger
+import java.io.IOException
+
+
 /** FlutterSmsServicePlugin */
-class FlutterSmsServicePlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class FlutterSmsServicePlugin : FlutterPlugin {
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_sms_service")
-    channel.setMethodCallHandler(this)
-  }
+    private lateinit var channel: MethodChannel
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    //    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    //        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_sms_service")
+    //        channel.setMethodCallHandler(this)
+    //    }
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        setupMethodChannel(binding.binaryMessenger, binding.applicationContext)
     }
-  }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+
+//    override fun onMethodCall(call: MethodCall, result: Result) {
+//        if (call.method == "getPlatformVersion") {
+//            result.success("Android ${android.os.Build.VERSION.RELEASE}")
+//        } else {
+//            result.notImplemented()
+//        }
+//    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    private fun setupMethodChannel(messenger: BinaryMessenger, context: Context) {
+        channel = MethodChannel(messenger, "flutter_sms_service")
+
+
+        val subscriptionManager: SubscriptionManager =
+            context.getSystemService<SubscriptionManager>(SubscriptionManager::class.java)
+
+        val handler = MethodCallHandlerImpl(subscriptionManager,context)
+        channel.setMethodCallHandler(handler)
+    }
 }
---
+++ android/src/main/kotlin/com/marj/flutter_sms_service/MethodCallHandlerImpl.kt
@@ -0,0 +1,40 @@
+package com.marj.flutter_sms_service
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.telephony.SubscriptionInfo
+import android.telephony.SubscriptionManager
+import androidx.core.app.ActivityCompat
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+
+
+internal class MethodCallHandlerImpl(
+    private val subscriptionManager: SubscriptionManager,
+    private val context: Context
+) : MethodCallHandler {
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when (call.method) {
+            "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
+            "getSubscriptionList" -> {
+
+            }
+
+            else -> result.notImplemented()
+
+        }
+    }
+
+    private fun getSubscriptionInfoLis(): List<SubscriptionInfo> {
+
+        val selfPermission: Int =
+            ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE)
+
+        val permissionGranted: Boolean = selfPermission == PackageManager.PERMISSION_GRANTED
+
+        return if (permissionGranted) subscriptionManager.activeSubscriptionInfoList else listOf<SubscriptionInfo>()
+    }
+
+}
